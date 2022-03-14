0cfb5fcd9b11c84c5caf0a502c2f172b5a4fd6ca
Olly Betts <olly@survex.com>
2022-03-15 12:36:07 +1300

    Fix --HEAD to actually use Perl local::lib

--- HomebrewFormula/survex.rb
+++ HomebrewFormula/survex.rb
@@ -46,9 +46,11 @@ class Survex < Formula
       ENV.prepend_path "PATH", "/usr/local/bin"
       ENV.prepend_path "PATH", "/opt/homebrew/bin"
       system "make", "-C", "lib/icons", "Aven.iconset.zip"
+      system 'eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib); make'
+    else
+      system "make"
     end
 
-    system "make"
     system "make", "install"
 
     # Create and populate Aven.app
